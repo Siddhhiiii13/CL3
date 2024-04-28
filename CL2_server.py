532ce410ddfcf6aa0d145bea82a0ea07cbb54ef5
Siddhhiiii13 <113752766+Siddhhiiii13@users.noreply.github.com>
2024-04-28 10:14:34 +0530

    Add files via upload

---
+++ CL2_server.py
@@ -0,0 +1,9 @@
+import Pyro4
+@Pyro4.expose
+class StringConcatenator:
+    def concatenate(self, str1, str2): 
+        return str1 + str2
+daemon = Pyro4.Daemon()
+uri = daemon.register(StringConcatenator)
+print("Server URI:", uri)
+daemon.requestLoop() 
